d4104cff9e8fa36111060b7079892e4cc4e0a069
uwmpr <uwmpr@proton.me>
2024-04-10 13:27:26 +0200

    filename

--- main.py
+++ main.py
@@ -2,13 +2,12 @@ from flask import Flask, request
 from file_send import  file_sender
 app = Flask(__name__)
 UPLOAD_FOLDER = "file_send"
-app.config['SERVER_NAME'] = '127.0.0.1:5545'
+app.config['SERVER_NAME'] = '192.168.0.7:5545'
 #app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-@app.route('/upload/<string:channel_id>/', methods=['POST'])
+@app.route('/<string:channel_id>/', methods=['POST'])
 async def upload(channel_id):
     data = request.headers
     auth = data.get('auth')
-    filename= data.get('filename')
     try:
         if 'file' not in request.files:
             return 'no file found', 500
@@ -18,7 +17,7 @@ async def upload(channel_id):
             return "files not selected", 500
 
         uploaded_file.save('file_send/' + uploaded_file.filename)
-        send = await file_sender.send_file(channel_id, auth, filename)
+        send = await file_sender.send_file(channel_id, auth, uploaded_file.filename)
         if(send == 200):
             return "file sent!", 200
         else:
